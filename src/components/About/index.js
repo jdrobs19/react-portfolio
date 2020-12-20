21ec15024a86730dab2b729788fc1aefe9d97010
Jordan Roberts <jdrobs19@gmail.com>
2020-12-20 15:16:59 -0700

    added about section

---
+++ src/components/About/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+
+function About() {
+    return (
+        <article id="about-me">
+            <div className="section-wrapper">
+                <div className="section-title section-title-wrapper">
+                    <h2>About Me</h2>
+                </div>
+                <div className="section-info">
+                    <p>I'm not good at talking about myself. I like to be outdoors, at times, but a majority of my time is spent with my wife and daughter. I play video games and watch movies.
+                    I'm working on bettering myself both physically and mentally. Currently, developing skills of a full-stack developer using the MERN stack. Enjoy front end work and database work.
+                    </p >
+                </div >
+        </div >
+    </article >
+    );
+}
+
+export default About;
